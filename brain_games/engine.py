ab9df65f44a87882b2426997d94cbad4cede92c9
Dmitry Baburin <d.baburin62@gmail.com>
2022-12-22 21:32:41 +0300

    refactoring

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,14 +1,14 @@
-from brain_games.engine_code.greet import welcome_user
-from brain_games.engine_code.even import even
+from brain_games.greet import welcome_user
+from brain_games.games import even
 from prompt import string
 
 
 def start():
     name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print(even.main_question)
     number_of_rounds = 3
     for _ in range(number_of_rounds):
-        question, correct_answer = even()
+        question, correct_answer = even.even()
         print(f"Question: {question}")
         answer = string('Your answer: ')
         if answer == correct_answer:
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,9 @@
 from random import randint
 
 
+main_question = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
 def even():
     question = randint(0, 100)
     if (question % 2 == 0):
