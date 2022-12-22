e6dcadfacb23cfb44ad8adbb9c3163fc740108bb
Dmitry Baburin <d.baburin62@gmail.com>
2022-12-23 00:09:52 +0300

    refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 main_question = 'What is the result of the expression?'
@@ -7,7 +7,7 @@ main_question = 'What is the result of the expression?'
 def run():
     num1 = randint(0, 30)
     num2 = randint(0, 30)
-    operator = ['+', '-', '*'][randint(0, 2)]
+    operator = choice(['+', '-', '*'])
     if operator == '+':
         return f'{num1} + {num2}', str(num1 + num2)
     if operator == '-':
