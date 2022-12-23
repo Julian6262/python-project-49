ff7e0d1a0647ee74cb33ad02c048603a4e13327b
Dmitry Baburin <d.baburin62@gmail.com>
2022-12-23 11:08:24 +0300

    refactoring

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,7 +12,7 @@ def run():
 
 
 def gcd(num1, num2):
-    if(num2 == 0):
+    if (num2 == 0):
         return num1
-    else: 
-        return gcd(num2, num1 % num2) 
+    else:
+        return gcd(num2, num1 % num2)
